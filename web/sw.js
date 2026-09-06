a675c0d0a326edae4b5ec37f75742eecbab0223e
johnksgyunas-systems <trimasmandiri1@gmail.com>
2026-09-06 22:19:00 +0800

    Add VEXA service worker

---
+++ web/sw.js
@@ -0,0 +1,5 @@
+const CACHE='vexa-v2.1';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
